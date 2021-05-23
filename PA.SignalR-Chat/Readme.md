24baa2f6f71e3829a4f5e98a5d184b85e8c3d9ad
ElectNewt <ivanxo69@gmail.com>
2021-05-23 23:37:38 +0100

    Ejemplo chat en tiempo real con SignalR

---
+++ PA.SignalR-Chat/Readme.md
@@ -0,0 +1,20 @@
+# Chat en tiempo real utilizando SignalR
+
+Con este proyecto puedes comprobar el funcionamiento de SignalR dentro de un chat en tiempo real. 
+
+## Cómo funciona
+
+Primero debes ejecutar el servidor, el cual es una aplicación de consola.
+
+Si la ejecutas desde el CLI (`dotnet run`) correra en `localhost:5000` esta url esta hardcoded en el cliente. 
+
+
+## Producción
+
+Si por algún casual has llegado aquí desde el curro, o para hacer algo en producción en tu trabajo. 
+No olvides, en `Program.cs` del servidor, configurar `CORS` correctamente.
+
+
+Enlace al blog con el proceso y el código explicado: `TODO`.
+
+Más tutoriales y explicaciones sobre código en .NET en [NetMentor](https://www.netmentor.es/)
---
+++ PA.SignalR-Chat/src/SignalRChat.Server/Program.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SignalRChat.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+        private static IWebHostBuilder CreateWebHostBuilder(string[] args) => WebHost.CreateDefaultBuilder(args).UseStartup<Startup>();
+
+    }
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration) { Configuration = configuration; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors();
+            services.AddSignalR();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            // global cors policy
+            app.UseCors(x => x
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .SetIsOriginAllowed(origin => true)); // allow any origin-only for development purposes
+            
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapHub<ChatHub>("/chat");
+            });
+        }
+    }
+}
