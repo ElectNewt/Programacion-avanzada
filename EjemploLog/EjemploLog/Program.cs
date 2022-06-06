388e3a3e0872b324ecccd2899e99b36e1c2c21a9
ElectNewt <ivanxo69@gmail.com>
2022-06-06 20:59:12 +0100

    add code for the blogpost about logs
    cleanup

---
+++ EjemploLog/EjemploLog/Program.cs
@@ -0,0 +1,31 @@
+using EjemploLog.Log;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddLogging(logger => logger.AddSerilog());
+
+builder.Host.ConfigureSerilog();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
